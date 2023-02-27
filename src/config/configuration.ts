26b6ba0b623f08388c9ce1b4f478703ca16d2f8a
Alex <bbqsauce108@gmail.com>
2023-02-27 23:17:10 +0400

    change access token expiration time

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -2,7 +2,7 @@ export default () => ({
   port: parseInt(process.env.PORT) || 5000,
   hashSalt: parseInt(process.env.HASH_SALT) || 13,
   jwtSecret: process.env.JWT_SECRET || '666',
-  jwtExpirationTime: process.env.JWT_EXPIRATION_TIME || '1h',
+  jwtExpirationTime: process.env.JWT_EXPIRATION_TIME || '10s',
   database: {
     MONGOOSE_URI: process.env.MONGOOSE_URI,
   },
